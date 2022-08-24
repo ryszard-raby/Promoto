8b8373928dd811e427211be6e8e4ac97dbfcb0d2
ryszard raby <ryszard.raby@gmail.com>
2022-08-24 18:36:30 +0200

    Offers

--- src/app/components/offer/offer.component.ts
+++ src/app/components/offer/offer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from 'src/app/services/firebase/firebase.service';
+import { LocationService } from 'src/app/services/location/location.service';
 
 @Component({
   selector: 'app-offer',
@@ -7,7 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfferComponent implements OnInit {
 
-  constructor() { }
+  header: string = '';
+  text: string = '';
+  offers: any[] = [];
+
+  constructor(firebase: FirebaseService, locationService: LocationService) {
+    locationService.activeLocation.subscribe(location => {
+      firebase.read('Content/Offer/Title/' + location).subscribe(title => {
+        this.header = title[0];
+        this.text = title[1];
+      })
+      firebase.read('Content/Offer/Offers/' + location).subscribe(offers => {
+        this.offers = offers
+      })
+    })
+  }
 
   ngOnInit(): void {
   }
